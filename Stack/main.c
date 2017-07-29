c8ad092980048f72281c7ab466318753f31d763e
smallX <xieyu559@163.com>
2017-07-29 12:33:35 +0800

    add files

--- Stack/main.c
+++ Stack/main.c
@@ -7,9 +7,48 @@
 //
 
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(void) {
+    char a[101],s[101];
+    int i, len, mid, next, top;
+    
+    gets(a);
+    len = strlen(a);
+    mid = len / 2 - 1;
+    
+    top = 0;
+    for(i = 0; i <= mid; i++)
+    {
+        s[++top] = a[i];
+    }
+    
+    if(len % 2 == 0)
+    {
+        next = mid + 1;
+    }
+    else
+    {
+        next = mid + 2;
+    }
+    
+    for(i = next; i<= len-1; i++)
+    {
+        if(a[i] != s[top])
+        {
+            break;
+        }
+        top--;
+    }
+    
+    if(top == 0)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+    getchar();getchar();
     return 0;
 }
